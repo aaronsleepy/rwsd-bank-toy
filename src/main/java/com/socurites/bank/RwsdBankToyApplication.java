85f00ccb664744e6754316b63397ed63f4f8a1cd
aaronsleepy <aaron@kmong.com>
2022-04-22 20:12:24 +0900

    first commit

---
+++ src/main/java/com/socurites/bank/RwsdBankToyApplication.java
@@ -0,0 +1,13 @@
+package com.socurites.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RwsdBankToyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RwsdBankToyApplication.class, args);
+    }
+
+}
