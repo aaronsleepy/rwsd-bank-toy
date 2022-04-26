623556bea433ddff20c80dc6ac87186c79f082da
aaronsleepy <aaron@kmong.com>
2022-04-26 20:14:49 +0900

    feature: selectInMonth 구현

--- src/main/java/com/socurites/bank/analyzer/BankTransactionAnalyzer.java
+++ src/main/java/com/socurites/bank/analyzer/BankTransactionAnalyzer.java
@@ -9,6 +9,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.Month;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class BankTransactionAnalyzer {
     public static final String RESOURCES = "src/main/resources/";
@@ -26,8 +27,12 @@ public class BankTransactionAnalyzer {
         System.out.println("The total for all transactions in January is " + selectInMonth(bankTransactions, Month.JANUARY));
     }
 
-    private static String selectInMonth(List<BankTransaction> bankTransactions, Month january) {
-        return null;
+    private static double selectInMonth(List<BankTransaction> bankTransactions, Month month) {
+        return calculateTotalAmount(
+                bankTransactions.stream()
+                        .filter(t -> month == t.getDate().getMonth())
+                        .collect(Collectors.toList())
+        );
     }
 
     private static double calculateTotalAmount(List<BankTransaction> bankTransactions) {
