8435b5e2ca93aa97a9b5734003bf98d4c34f9d5d
aaronsleepy <aaron@kmong.com>
2022-04-26 20:08:18 +0900

    refactoring: BankStatementCSVParser 사용하도록 변경

---
+++ src/main/java/com/socurites/bank/analyzer/BankTransactionAnalyzer.java
@@ -0,0 +1,36 @@
+package com.socurites.bank.analyzer;
+
+import com.socurites.bank.domain.BankTransaction;
+import com.socurites.bank.parser.BankStatementCSVParser;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.Month;
+import java.util.List;
+
+public class BankTransactionAnalyzer {
+    public static final String RESOURCES = "src/main/resources/";
+
+    public static void main(String[] args) throws IOException {
+        final BankStatementCSVParser bankStatementParser = new BankStatementCSVParser();
+
+        final String fileName = args[0];
+        final Path path = Paths.get(RESOURCES + fileName);
+        List<String> lines = Files.readAllLines(path);
+
+        final List<BankTransaction> bankTransactions = bankStatementParser.parseLinesFromCSV(lines);
+
+        System.out.println("The total for all transactions is " + caclulateTotalAmount(bankTransactions));
+        System.out.println("The total for all transactions in January is " + selectInMonth(bankTransactions, Month.JANUARY));
+    }
+
+    private static String selectInMonth(List<BankTransaction> bankTransactions, Month january) {
+        return null;
+    }
+
+    private static String caclulateTotalAmount(List<BankTransaction> bankTransactions) {
+        return null;
+    }
+}
