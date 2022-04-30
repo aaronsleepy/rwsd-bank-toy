6b90371aa57b9f4845fc0e6930bd452c08e01ed5
aaronsleepy <aaron@kmong.com>
2022-04-30 21:03:16 +0900

    feature: BankStatementParser 인터페이스 추가

---
+++ src/main/java/com/socurites/bank/parser/BankStatementParser.java
@@ -0,0 +1,10 @@
+package com.socurites.bank.parser;
+
+import com.socurites.bank.domain.BankTransaction;
+
+import java.util.List;
+
+public interface BankStatementParser {
+    BankTransaction parseFrom(String line);
+    List<BankTransaction> parseLinesFrom(List<String> lines);
+}
