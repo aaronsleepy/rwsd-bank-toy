e2358efb0a0aa6377aabab520b1c4ecd93f200ee
aaronsleepy <aaron@kmong.com>
2022-05-01 20:26:56 +0900

    refactoring: summarizeTransactions()와 Exporter 사용하도록 수정

--- src/main/java/com/socurites/bank/analyzer/BankStatementAnalyzer.java
+++ src/main/java/com/socurites/bank/analyzer/BankStatementAnalyzer.java
@@ -1,9 +1,12 @@
 package com.socurites.bank.analyzer;
 
 import com.socurites.bank.domain.BankTransaction;
+import com.socurites.bank.domain.SummaryStatistics;
 import com.socurites.bank.parser.BankStatementCSVParser;
 import com.socurites.bank.parser.BankStatementParser;
 import com.socurites.bank.processor.BankStatementProcessor;
+import com.socurites.bank.reporter.Exporter;
+import com.socurites.bank.reporter.HtmlExporter;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -16,10 +19,10 @@ public class BankStatementAnalyzer {
     public static final String RESOURCES = "src/main/resources/";
 
     public static void main(String[] args) throws IOException {
-        analyze(args[0], new BankStatementCSVParser());
+        analyze(args[0], new BankStatementCSVParser(), new HtmlExporter());
     }
 
-    private static void analyze(String fileName, BankStatementParser bankStatementParser) throws IOException {
+    private static void analyze(String fileName, BankStatementParser bankStatementParser, Exporter exporter) throws IOException {
 
         final Path path = Paths.get(RESOURCES + fileName);
         List<String> lines = Files.readAllLines(path);
@@ -27,12 +30,12 @@ public class BankStatementAnalyzer {
         final List<BankTransaction> bankTransactions = bankStatementParser.parseLinesFrom(lines);
 
         BankStatementProcessor bankStatementProcessor = new BankStatementProcessor(bankTransactions);
-        collectSummary(bankStatementProcessor);
+        SummaryStatistics summaryStatistics = bankStatementProcessor.summarizeTransactions();
+
+        collectSummary(exporter, summaryStatistics);
     }
 
-    private static void collectSummary(final BankStatementProcessor bankStatementProcessor) {
-        System.out.println("The total for all transactions is " + bankStatementProcessor.calculateTotalAmount());
-        System.out.println("The total for all transactions in January is " + bankStatementProcessor.calculateTotalInMonth(Month.JANUARY));
-        System.out.println("The total salary received is " + bankStatementProcessor.calculateTotalForCategory("Salary"));
+    private static void collectSummary(final Exporter exporter, final SummaryStatistics summaryStatistics) {
+        System.out.println(exporter.export(summaryStatistics));
     }
 }
